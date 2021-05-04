98e7d72acbe7ed1a94ded52e99fa85d2ab792967
Doğan Can Selek <canselek2434@gmail.com>
2021-05-05 01:24:16 +0300

    Update userInfo.js

--- src/components/user/userInfo.js
+++ src/components/user/userInfo.js
@@ -6,9 +6,11 @@ export default function UserInfo() {
 
     const { info } = useSelector((state) => state.userInfo);
 
+    console.log(info[0].payload.isim)
+
     return(
         <View style={{flex: 1}}>
-            <Text style={{color: "white"}} >{info[0].isim+ " " + info[0].soyisim}</Text>
+            <Text style={{color: "white"}} >{info[0].payload.isim+ " " + info[0].payload.soyisim}</Text>
         </View>
     )
 }
