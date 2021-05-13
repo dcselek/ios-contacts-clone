c5135f862e66685ebc71c211d32ff4f5ce6c0c91
Doğan Can Selek <canselek2434@gmail.com>
2021-05-14 00:05:44 +0300

    delete animation eklendi

--- src/components/modals/newContact.js
+++ src/components/modals/newContact.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { StyleSheet, Text, View, Pressable, Modal, TouchableOpacity, TextInput, TouchableHighlight, ScrollView, SafeAreaView, Animated, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Modal, TouchableOpacity, TextInput, TouchableHighlight, ScrollView, SafeAreaView, Animated, TouchableWithoutFeedback, Keyboard, Easing } from 'react-native';
 import SvgAvatarPerson1 from '../icons/AvatarPerson1'
 
 import { addFirstName } from '../../redux/addContact'
@@ -7,27 +7,105 @@ import { useDispatch } from 'react-redux'
 
 export default function NewContact({ setModalVisible, modalVisible }) {
 
+    const [deleteT, setDeleteT] = useState("");
+
     const [name, setName] = useState("");
     const [surname, setSurname] = useState("");
     const [phone, setPhone] = useState("");
 
-    const upAnim = useRef(new Animated.Value(0)).current;
+    const inputAnim = useRef(new Animated.Value(0)).current;
+    const buttonAnim = useRef(new Animated.Value(0)).current;
     const scaleAnim = useRef(new Animated.Value(0)).current;
+    const slideLeft = useRef(new Animated.Value(0)).current;
+    const scaleClose = useRef(new Animated.Value(0)).current;
 
     const upCome = () => {
-        Animated.parallel([
-            Animated.timing(upAnim, {
-                toValue: 15,
-                duration: 300,
+        Animated.sequence([
+            Animated.timing(scaleAnim, {
+                toValue: 1,
+                duration: 100,
                 useNativeDriver: true,
             }),
-            Animated.timing(scaleAnim, {
+            Animated.parallel([
+                Animated.timing(inputAnim, {
+                    toValue: 15,
+                    duration: 200,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(buttonAnim, {
+                    toValue: 15,
+                    duration: 200,
+                    useNativeDriver: true,
+                }),
+            ]),
+
+            Animated.timing(scaleClose, {
                 toValue: 1,
                 duration: 100,
                 useNativeDriver: true
             })
+
         ]).start();
     }
+
+    const slideL = () => {
+        Animated.timing(slideLeft, {
+            toValue: -80,
+            duration: 200,
+            useNativeDriver: true
+        }).start();
+    }
+
+    const closeD = () => {
+        Animated.sequence([
+            Animated.parallel([
+                Animated.timing(buttonAnim, {
+                    toValue: 5,
+                    duration: 100,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(scaleClose, {
+                    toValue: 100,
+                    duration: 100,
+                    useNativeDriver: true
+                }),
+                Animated.timing(slideLeft, {
+                    toValue: 0,
+                    duration: 50,
+                    useNativeDriver: true
+                }),
+            ]),
+            Animated.parallel([
+                Animated.timing(inputAnim, {
+                    toValue: 0,
+                    duration: 300,
+                    useNativeDriver: true
+                }),
+                Animated.timing(scaleAnim, {
+                    toValue: 0,
+                    duration: 200,
+                    useNativeDriver: true,
+                }),
+            ]),
+            Animated.timing(scaleClose, {
+                toValue: 0,
+                duration: 50,
+                useNativeDriver: true
+            }),
+            Animated.timing(slideLeft, {
+                toValue: 0,
+                duration: 50,
+                useNativeDriver: true
+            }),
+            Animated.timing(scaleClose, {
+                toValue: 0,
+                duration: 50,
+                useNativeDriver: true
+            }),
+            
+        ]).start();
+    }
+
 
 
     const SELECTITEMS = [
@@ -93,17 +171,26 @@ export default function NewContact({ setModalVisible, modalVisible }) {
                     <View>
 
                         <View>
-                            <Animated.View style={[styles.selectContainer, { borderBottomWidth: 0.7, borderBottomColor: "#3a3a3c", opacity: upAnim, marginTop: 26, flexDirection: "row", alignItems: "center", transform: [{ scaleY: scaleAnim }, { translateY: upAnim }] }]}>
-                                <Pressable>
-                                    <View style={[styles.selectIconContainer, { backgroundColor: "red", marginLeft: 24 }]}>
-                                        <Text style={{ fontSize: 18, color: "white", fontWeight: '500' }}>-</Text>
-                                    </View>
-                                </Pressable>
-                                <Text style={[styles.selectIconTitle, { color: "#0a84ff" }]}>mobile</Text>
-                                <TextInput placeholder="Phone" placeholderTextColor="#8e8e93" style={styles.inputStyle} keyboardType= "numeric" onChangeText={(word) => { setPhone(word); }} />
-                            </Animated.View>
-                            <Animated.View style={{ transform: [{ translateY: upAnim }] }}>
-                                <TouchableHighlight style={[styles.selectContainer]} underlayColor='#3a3a3c' onPress={() => { upCome(); }}>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 26 }}>
+                                <Animated.View style={[styles.selectContainer, { borderBottomWidth: 0.7, borderBottomColor: "#3a3a3c", opacity: inputAnim, flexDirection: "row", alignItems: "center", transform: [{ scaleY: scaleAnim }, { translateY: inputAnim }, { translateX: slideLeft }] }]}>
+                                    <Pressable onPress={() => slideL()}>
+                                        <View style={[styles.selectIconContainer, { backgroundColor: "#ff3b30", marginLeft: 24 }]}>
+                                            <Text style={{ fontSize: 18, color: "white", fontWeight: '500' }}>-</Text>
+                                        </View>
+                                    </Pressable>
+                                    <Text style={[styles.selectIconTitle, { color: "#0a84ff" }]}>mobile</Text>
+                                    <TextInput placeholder="Phone" placeholderTextColor="#8e8e93" style={styles.inputStyle} keyboardType="numeric" onChangeText={(word) => { setPhone(word); }} />
+                                </Animated.View>
+                                <Animated.View style={{ backgroundColor: "#ff3b30", opacity: inputAnim, transform: [{ scaleY: scaleAnim }, { scaleX: scaleClose }, { translateY: inputAnim }, { translateX: slideLeft }] }}>
+                                    <Pressable onPress={() => {closeD(); setDeleteT("");}}>
+                                        <View style={{ paddingVertical: 13, paddingHorizontal: 16, alignItems: 'center', justifyContent: 'center' }}>
+                                            <Text style={{ fontSize: 16, color: "white", fontWeight: '500' }}>{deleteT}</Text>
+                                        </View>
+                                    </Pressable>
+                                </Animated.View>
+                            </View>
+                            <Animated.View style={{ transform: [{ translateY: buttonAnim }] }}>
+                                <TouchableHighlight style={[styles.selectContainer]} underlayColor='#3a3a3c' onPress={() => { upCome(); setDeleteT("Delete"); }}>
                                     <View style={{ flexDirection: 'row', paddingLeft: 24, alignItems: 'center' }}>
                                         <View style={styles.selectIconContainer}>
                                             <Text style={styles.selectIcon}>+</Text>
@@ -111,9 +198,10 @@ export default function NewContact({ setModalVisible, modalVisible }) {
                                         <Text style={styles.selectIconTitle}>add phone</Text>
                                     </View>
                                 </TouchableHighlight>
+
                             </Animated.View>
                         </View>
-                        {SELECTITEMS.map((items) => {
+                        {/* {SELECTITEMS.map((items) => {
                             return (<TouchableHighlight key={items.name} style={[styles.selectContainer, { marginTop: 42 }]} underlayColor='#3a3a3c' onPress={() => null}>
                                 <View style={{ flexDirection: 'row', paddingLeft: 24, alignItems: 'center' }}>
                                     <View style={styles.selectIconContainer}>
@@ -122,7 +210,7 @@ export default function NewContact({ setModalVisible, modalVisible }) {
                                     <Text style={styles.selectIconTitle}>add {items.name}</Text>
                                 </View>
                             </TouchableHighlight>)
-                        })}
+                        })} */}
 
 
                     </View>
